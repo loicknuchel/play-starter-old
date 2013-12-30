6feadcd12b542202dceb7dda27f8c9ba7f536f81
Loïc Knuchel <loicknuchel@gmail.com>
2013-12-30 21:10:00 +0100

    improve Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -6,9 +6,9 @@ object ApplicationBuild extends Build {
   val appName = "play-starter"
   val appVersion = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    "securesocial" %% "securesocial" % "master-SNAPSHOT")
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns))
+  val main = play.Project(appName, appVersion).settings(
+    resolvers ++= Seq(
+      Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)),
+    libraryDependencies ++= Seq(
+      "securesocial" %% "securesocial" % "master-SNAPSHOT"))
 }
